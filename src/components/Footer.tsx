56b5eb8d5399577358432b2a7a78b3a2315ca67e
Bon12-Rein <bonkereinhard45@gmail.com>
2025-07-08 18:47:26 +0300

    Add TikTok icon back to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Facebook, Twitter, Youtube, Linkedin, Instagram } from 'lucide-react';
 
+// Custom TikTok icon component
+const TikTokIcon = ({ size = 18 }: { size?: number }) => (
+  <svg width={size} height={size} viewBox="0 0 24 24" fill="currentColor">
+    <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"/>
+  </svg>
+);
+
 const Footer: React.FC = () => {
   const { t } = useTranslation();
   
@@ -10,7 +17,8 @@ const Footer: React.FC = () => {
     { icon: <Twitter size={18} />, name: 'Twitter', url: 'https://twitter.com/telispacdigital' },
     { icon: <Youtube size={18} />, name: 'YouTube', url: 'https://youtube.com/@telispacdigital' },
     { icon: <Linkedin size={18} />, name: 'LinkedIn', url: 'https://linkedin.com/company/telispacdigital' },
-    { icon: <Instagram size={18} />, name: 'Instagram', url: 'https://instagram.com/telispacdigital' }
+    { icon: <Instagram size={18} />, name: 'Instagram', url: 'https://instagram.com/telispacdigital' },
+    { icon: <TikTokIcon size={18} />, name: 'TikTok', url: 'https://tiktok.com/@telispacdigital' }
   ];
 
   const resourceLinks = [
